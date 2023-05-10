046201d67a339d6402616eff9245b6093a741c9e
w.wouwenberg <w.wouwenberg@voortman.net>
2023-05-10 16:15:06 +0200

    Initial version

---
+++ Battleships/FieldValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Battleships
+{
+    public static class FieldValidator
+    {
+        public static bool ValidateField(int[,] field)
+        {
+            //TODO: Implement either version of the rules, with or without adjacent ships. Pick the first test class for the rules without adjacency. 
+            return false;
+        }
+    }
+}
